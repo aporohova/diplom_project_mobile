1a8141151f6d9a354c0cd562a01e6e97f1af835b
alena.porohova <alena.porohova@lamoda.ru>
2023-06-20 21:45:41 +0300

    add Tags

--- src/test/java/tests/OpenArticleTest.java
+++ src/test/java/tests/OpenArticleTest.java
@@ -3,11 +3,13 @@ package tests;
 import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
+@Tag("android")
 public class OpenArticleTest extends TestBase{
     @Test
     @DisplayName("Successful wiki search")
